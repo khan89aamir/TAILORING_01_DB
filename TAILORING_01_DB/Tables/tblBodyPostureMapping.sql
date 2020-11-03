45acc01bfce94beda025e8d9fad12539e532c441
AAMIR <AAMIR@KHAN>
2020-11-04 03:11:10 +0530

    1. Added two tables for maintaining Customer's body postures.

---
+++ TAILORING_01_DB/Tables/tblBodyPostureMapping.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblBodyPostureMapping](
+	[BodyPostureMappingID] [int] IDENTITY(1,1) NOT NULL,
+	[BodyPostureID] [int] NOT NULL,
+	[BodyPostureImage] [varchar](100) NOT NULL,
+ CONSTRAINT [PK_tblBodyPostureMapping] PRIMARY KEY CLUSTERED 
+(
+	[BodyPostureMappingID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
---
+++ TAILORING_01_DB/Tables/tblBodyPostureMaster.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblBodyPostureMaster](
+	[BodyPostureID] [int] IDENTITY(1,1) NOT NULL,
+	[BodyPostureType] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_tblBodyPostureMaster] PRIMARY KEY CLUSTERED 
+(
+	[BodyPostureID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
