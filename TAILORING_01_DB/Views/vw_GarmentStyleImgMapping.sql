3968ade3a82c5275bc9e0c01208481276ff2b15e
AAMIR <khan89aamir@gmail.com>
2020-12-24 14:23:25 +0530

    1.added views 2. added isdefault column in company master table.

---
+++ TAILORING_01_DB/Views/vw_GarmentStyleImgMapping.sql
@@ -0,0 +1,5 @@
+﻿SELECT        pm.GarmentID, pm.GarmentName, SM.StyleID, SM.StyleName, sim.ImageName, sim.StyleImageID
+FROM            dbo.GarmentStyleMapping AS gsm INNER JOIN
+                         dbo.tblStyleMaster AS SM ON gsm.StyleID = SM.StyleID INNER JOIN
+                         dbo.tblProductMaster AS pm ON gsm.GarmentID = pm.GarmentID INNER JOIN
+                         dbo.tblStyleImageMaster AS sim ON pm.GarmentID = sim.GarmentID AND SM.StyleID = sim.StyleID
