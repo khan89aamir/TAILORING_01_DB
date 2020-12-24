7580e71db2694b08ded6054b66fbc0d0bf6da084
AAMIR <khan89aamir@gmail.com>
2020-12-25 02:52:55 +0530

    1 added create view keyword for views

--- TAILORING_01_DB/Views/vw_GarmentStyleImgMapping.sql
+++ TAILORING_01_DB/Views/vw_GarmentStyleImgMapping.sql
@@ -1,5 +1,9 @@
-﻿SELECT        pm.GarmentID, pm.GarmentName, SM.StyleID, SM.StyleName, sim.ImageName, sim.StyleImageID
+﻿CREATE VIEW [dbo].[vw_GarmentStyleImgMapping]
+AS
+SELECT        pm.GarmentID, pm.GarmentName, SM.StyleID, SM.StyleName, sim.ImageName, sim.StyleImageID
 FROM            dbo.GarmentStyleMapping AS gsm INNER JOIN
                          dbo.tblStyleMaster AS SM ON gsm.StyleID = SM.StyleID INNER JOIN
                          dbo.tblProductMaster AS pm ON gsm.GarmentID = pm.GarmentID INNER JOIN
                          dbo.tblStyleImageMaster AS sim ON pm.GarmentID = sim.GarmentID AND SM.StyleID = sim.StyleID
+
+GO
--- TAILORING_01_DB/Views/vw_GarmentStyleMapping.sql
+++ TAILORING_01_DB/Views/vw_GarmentStyleMapping.sql
@@ -1,4 +1,8 @@
-﻿SELECT        pm.GarmentID, pm.GarmentName, SM.StyleID, SM.StyleName
+﻿CREATE VIEW [dbo].[vw_GarmentStyleMapping]
+AS
+SELECT        pm.GarmentID, pm.GarmentName, SM.StyleID, SM.StyleName
 FROM            dbo.GarmentStyleMapping AS gsm INNER JOIN
                          dbo.tblStyleMaster AS SM ON gsm.StyleID = SM.StyleID INNER JOIN
                          dbo.tblProductMaster AS pm ON gsm.GarmentID = pm.GarmentID
+
+GO
