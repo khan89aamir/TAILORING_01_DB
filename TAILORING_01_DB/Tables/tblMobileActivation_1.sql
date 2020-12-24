c3b76f3a01ecff5d453156a26508fbaf54da9b4a
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-12-24 22:31:41 +0530

    mobile script changes

---
+++ TAILORING_01_DB/Tables/tblMobileActivation_1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblMobileActivation](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SerialNumber] [nvarchar](50) NULL,
+	[ActivationCode] [nvarchar](50) NULL,
+	[CreatedOn] [datetime] NULL DEFAULT (getdate()),
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
