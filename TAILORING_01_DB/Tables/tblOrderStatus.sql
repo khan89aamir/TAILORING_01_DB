35852668742905e7b2ed5d4b364e592119716bc1
Abdul Mateen Khan <abdulmateen50@gmail.com>
2021-01-23 01:39:35 +0530

    order status table created

---
+++ TAILORING_01_DB/Tables/tblOrderStatus.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblOrderStatus]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SalesOrderID] INT NULL, 
+    [SalesOrderDetailsID] INT NULL, 
+    [GarmentID] INT NULL, 
+    [OrderStatus] INT NULL
+)
---
+++ TAILORING_01_DB/Tables/tblOrderStatusMaster.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblOrderStatusMaster]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [OrderStatus] NVARCHAR(50) NULL
+)
+
+
