fa8e11a8c5591eb0dffa8fdd8faeb89c9f6ab480
Abdul Mateen Khan <abdulmateen50@gmail.com>
2021-01-25 14:31:58 +0530

    changes in view orderstatusdetails

--- TAILORING_01_DB/Views/vw_GetOrderStatusDetails.sql
+++ TAILORING_01_DB/Views/vw_GetOrderStatusDetails.sql
@@ -5,8 +5,13 @@ SELECT so.SalesOrderID,sd.SalesOrderDetailsID,pm.GarmentName,pm.GarmentID,pm.Gar
 	(CASE sd.[Service] WHEN 1 THEN 'Urgent' WHEN 0 THEN 'Normal' END) [Service]
 	,IIF(sd.TrailDate='1900-01-01',NULL,sd.TrailDate) TrailDate,sd.DeliveryDate,
 	(sd.QTY*sd.Rate) as Total,  
-	(case  when os.OrderStatus IS NULL  then 'NA' when os.OrderStatus=3 then 'In Process'
-	 when os.OrderStatus=4 then 'Received' end)
+	(case  when os.OrderStatus IS NULL  then 'NA'
+		  when os.OrderStatus=3 then 'In Process'
+	      when os.OrderStatus=4 then 'Received'
+		  when os.OrderStatus=1 then 'Delivered'
+		   when os.OrderStatus=2 then 'Critical'
+		  
+		  end)
 	as OrderStatus
 	FROM [dbo].[tblSalesOrder] so
 	INNER JOIN [dbo].[tblSalesOrderDetails] sd ON so.SalesOrderID=sd.SalesOrderID
