96b3d57e682b293d184ba3842034df97e3f9117d
Abdul Mateen Khan <abdulmateen50@gmail.com>
2021-01-27 02:17:19 +0530

    dfdf

---
+++ TAILORING_01_DB/Views/vw_Chalan_Rdlc.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vw_Chalan_Rdlc]
+as
+  select  so.SalesOrderID , sod.SubOrderNo,pm.GarmentName,stm.StichTypeName as StichType,ftm.FitTypeName as FitType,'Normal' as ServiceType,sod.TrailDate as gTrailDate,sod.DeliveryDate,sod.QTY,sod.rate,so.OrderNo from tblSalesOrder so join tblSalesOrderDetails sod
+  on so.SalesOrderID=sod.SalesOrderID join 
+ tblProductMaster pm on sod.GarmentID=pm.GarmentID
+ join tblStichTypeMaster stm on sod.StichTypeID=stm.StichTypeID 
+ join tblFitTypeMaster ftm on sod.FitTypeID=ftm.FitTypeID
+
