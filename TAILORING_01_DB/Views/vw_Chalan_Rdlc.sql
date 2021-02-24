9484389727d7c20a56f1f1318adf8ffbe29c28bf
AAMIR <khan89aamir@gmail.com>
2021-02-24 20:56:22 +0530

    added trail date blank if default value added dynamic service type from order details

--- TAILORING_01_DB/Views/vw_Chalan_Rdlc.sql
+++ TAILORING_01_DB/Views/vw_Chalan_Rdlc.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[vw_Chalan_Rdlc]
 as
-  SELECT  so.SalesOrderID , sod.SubOrderNo,pm.GarmentName,stm.StichTypeName as StichType,ftm.FitTypeName as FitType,'Normal' as ServiceType,sod.TrailDate as gTrailDate,sod.DeliveryDate,sod.QTY,sod.Rate,so.OrderNo 
+  SELECT  so.SalesOrderID ,so.OrderNo,sod.SubOrderNo,pm.GarmentName,stm.StichTypeName as StichType,ftm.FitTypeName as FitType,(CASE sod.[Service] WHEN 1 THEN 'Urgent' WHEN 0 THEN 'Normal' END) as ServiceType,CAST(NULLIF (sod.TrailDate, '') AS DATE) as gTrailDate,sod.DeliveryDate,sod.QTY,sod.Rate 
   from tblSalesOrder so 
   join tblSalesOrderDetails sod on so.SalesOrderID=sod.SalesOrderID 
   join tblProductMaster pm on sod.GarmentID=pm.GarmentID
--- TAILORING_01_DB/Views/vw_OrderDetails_RDLC.sql
+++ TAILORING_01_DB/Views/vw_OrderDetails_RDLC.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[vw_OrderDetails_RDLC]
 as
 
- SELECT  so.SalesOrderID , sod.SubOrderNo,pm.GarmentName,stm.StichTypeName AS StichType,ftm.FitTypeName AS FitType,(CASE sod.[Service] WHEN 1 THEN 'Urgent' WHEN 0 THEN 'Normal' END) AS ServiceType, CAST(NULLIF( sod.TrailDate,'') AS DATE)  as gTrailDate,sod.DeliveryDate,sod.Rate,so.OrderNo,
+ SELECT  so.SalesOrderID ,so.OrderNo, sod.SubOrderNo,pm.GarmentName,stm.StichTypeName AS StichType,ftm.FitTypeName AS FitType,(CASE sod.[Service] WHEN 1 THEN 'Urgent' WHEN 0 THEN 'Normal' END) AS ServiceType, CAST(NULLIF( sod.TrailDate,'') AS DATE)  as gTrailDate,sod.DeliveryDate,sod.Rate,
   osm.OrderStatus,os.ReceivedDate,os.DeliveredDate,
   (SELECT [Name] FROM EmployeeDetails WITH(NOLOCK) WHERE EmpID=os.ReceivedBy ) AS ReceivedBy, 
   (SELECT [Name] FROM EmployeeDetails WITH(NOLOCK) WHERE EmpID=os.DeliveredBy ) AS DeliveredBy
