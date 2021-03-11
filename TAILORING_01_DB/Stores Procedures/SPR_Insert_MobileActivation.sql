4902758f735e8fe58d2666dcd503c04390ee4fd2
AAMIR <khan89aamir@gmail.com>
2021-03-12 00:40:16 +0530

    1. Added SP SPR_Sync_Customer 2. Added SP SPR_Insert_MobileActivation

---
+++ TAILORING_01_DB/Stores Procedures/SPR_Insert_MobileActivation.sql
@@ -0,0 +1,57 @@
+﻿-- =============================================
+-- Author:		<AAMIR>
+-- Create date: <11th MAR 2021>
+-- Update date: <>
+-- Description:	<Description,,>
+-- =============================================
+--EXEC SPR_Insert_MobileActivation 'serialno','activation code'
+CREATE PROCEDURE SPR_Insert_MobileActivation
+@SerialNumber	nvarchar(max)=''
+,@ActivationCode	nvarchar(max)=''
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+	DECLARE @PARAMERES VARCHAR(MAX)=''
+	SET @PARAMERES=CONCAT(@SerialNumber,',',@ActivationCode)
+
+	BEGIN TRANSACTION
+
+
+	INSERT INTO tblMobileActivation
+	(SerialNumber,ActivationCode)
+	VALUES(@SerialNumber,@ActivationCode)
+
+	COMMIT
+
+	END TRY
+
+	BEGIN CATCH
+	
+	ROLLBACK
+
+	INSERT [dbo].[ERROR_Log]
+	(
+	ERR_NUMBER
+	, ERR_SEVERITY
+	, ERR_STATE
+	, ERR_LINE
+	, ERR_MESSAGE
+	, ERR_PROCEDURE
+	, PARAMERES
+	)
+	SELECT  
+	ERROR_NUMBER() 
+	,ERROR_SEVERITY() 
+	,ERROR_STATE() 
+	,ERROR_LINE()
+	,ERROR_MESSAGE()
+	,ERROR_PROCEDURE()
+	,@PARAMERES
+	
+	END CATCH
+END
+GO
---
+++ TAILORING_01_DB/Stores Procedures/SPR_Sync_Customer.sql
@@ -0,0 +1,52 @@
+﻿-- =============================================
+-- Author:		<AAMIR>
+-- Create date: <11th MAR 2021>
+-- Update date: <>
+-- Description:	<Description,,>
+-- =============================================
+--EXEC SPR_Sync_Customer 2021
+CREATE PROCEDURE SPR_Sync_Customer
+@LastChange INT=0
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+	DECLARE @PARAMERES VARCHAR(MAX)=''
+	SET @PARAMERES=@LastChange
+
+	SELECT CustomerID,[Name],[Address],MobileNo,EmailID,CONVERT(INT,LastChange) LastChange
+	FROM CustomerMaster WITH(NOLOCK)
+	WHERE CONVERT(INT,LastChange)=@LastChange
+
+	END TRY
+
+	BEGIN CATCH
+	
+	ROLLBACK
+
+	INSERT [dbo].[ERROR_Log]
+	(
+	ERR_NUMBER
+	, ERR_SEVERITY
+	, ERR_STATE
+	, ERR_LINE
+	, ERR_MESSAGE
+	, ERR_PROCEDURE
+	, PARAMERES
+	)
+	SELECT  
+	ERROR_NUMBER() 
+	,ERROR_SEVERITY() 
+	,ERROR_STATE() 
+	,ERROR_LINE()
+	,ERROR_MESSAGE()
+	,ERROR_PROCEDURE()
+	,@PARAMERES
+	
+	END CATCH
+
+END
+GO
