65155383ec04c85c25a19207e722cbf27690c955
AAMIR <khan89aamir@gmail.com>
2021-03-24 13:39:05 +0530

    Added OrderDate column in view vw_OrderDetails_RDLC

--- TAILORING_01_DB/Views/vw_OrderDetails_RDLC.sql
+++ TAILORING_01_DB/Views/vw_OrderDetails_RDLC.sql
@@ -1,15 +1,15 @@
 ﻿CREATE VIEW [dbo].[vw_OrderDetails_RDLC]
 as
 
- SELECT  so.SalesOrderID ,so.OrderNo, sod.SubOrderNo,pm.GarmentName,stm.StichTypeName AS StichType,ftm.FitTypeName AS FitType,(CASE sod.[Service] WHEN 1 THEN 'Urgent' WHEN 0 THEN 'Normal' END) AS ServiceType, CAST(NULLIF( sod.TrailDate,'') AS DATE)  as gTrailDate,sod.DeliveryDate,sod.Rate,
+ SELECT  so.SalesOrderID ,so.OrderNo, sod.SubOrderNo,so.OrderDate,pm.GarmentName,stm.StichTypeName AS StichType,ftm.FitTypeName AS FitType,(CASE sod.[Service] WHEN 1 THEN 'Urgent' WHEN 0 THEN 'Normal' END) AS ServiceType, CAST(NULLIF( sod.TrailDate,'') AS DATE)  as gTrailDate,sod.DeliveryDate,sod.Rate,
   osm.OrderStatus,os.ReceivedDate,os.DeliveredDate,
   (SELECT [Name] FROM EmployeeDetails WITH(NOLOCK) WHERE EmpID=os.ReceivedBy ) AS ReceivedBy, 
   (SELECT [Name] FROM EmployeeDetails WITH(NOLOCK) WHERE EmpID=os.DeliveredBy ) AS DeliveredBy
 
   FROM tblSalesOrder so 
-  join tblSalesOrderDetails sod ON so.SalesOrderID=sod.SalesOrderID 
-  join tblOrderStatus os ON sod.SalesOrderDetailsID=os.SalesOrderDetailsID 
-  join tblProductMaster pm ON sod.GarmentID=pm.GarmentID
-  join tblStichTypeMaster stm ON sod.StichTypeID=stm.StichTypeID 
-  join tblFitTypeMaster ftm ON sod.FitTypeID=ftm.FitTypeID
-  join tblOrderStatusMaster osm ON os.OrderStatus=osm.Id
+  JOIN tblSalesOrderDetails sod ON so.SalesOrderID=sod.SalesOrderID 
+  JOIN tblOrderStatus os ON sod.SalesOrderDetailsID=os.SalesOrderDetailsID 
+  JOIN tblProductMaster pm ON sod.GarmentID=pm.GarmentID
+  JOIN tblStichTypeMaster stm ON sod.StichTypeID=stm.StichTypeID 
+  JOIN tblFitTypeMaster ftm ON sod.FitTypeID=ftm.FitTypeID
+  JOIN tblOrderStatusMaster osm ON os.OrderStatus=osm.Id
